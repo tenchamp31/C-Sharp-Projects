93932871ff3f4c06ef304f0b805dceb4bf7d7747
tenchamp31 <ygandhi191@gmail.com>
2021-08-19 15:21:04 -0400

    While_Do statement

---
+++ While_Do statement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Do_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;           //While statement
+            while (x < 20)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+
+            int y = 0;
+            do
+            {
+                Console.WriteLine(y);               //do while statement
+                y++;
+            }
+            while (y < 15);
+
+            Console.ReadLine();
+        }
+    }
+}
