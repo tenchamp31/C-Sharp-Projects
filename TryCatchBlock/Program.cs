4d433950bd494b81572a062aa0d22ba996aa211f
tenchamp31 <ygandhi191@gmail.com>
2021-11-08 11:02:24 -0500

    Fixing Assignment

--- TryCatchBlock/Program.cs
+++ TryCatchBlock/Program.cs
@@ -10,26 +10,26 @@ namespace TryCatchBlock
     {
         static void Main(string[] args)
         {
-            List<int> Integers = new List<int> { 6, 4, 7 };
+            List<int> Integers = new List<int> { 6, 4, 7 }; //Making integer list
             try
             {
-                Console.WriteLine("What is your fav number");
+                Console.WriteLine("What is your fav number"); //Asking number to divide by
                 int answer1 = int.Parse(Console.ReadLine());
 
 
                 for (int i = 0; i < Integers.Count; i++)
-                    Console.WriteLine(Integers[i] / answer1);
+                    Console.WriteLine(Integers[i] / answer1);  //Displaying remainders
                 Console.ReadLine();
 
             }
 
-            catch (DivideByZeroException s)
+            catch (DivideByZeroException s) //Divide by zero message
             {
                 Console.WriteLine(s.Message);
 
             }
 
-            catch (FormatException y)
+            catch (FormatException y)  //Format message
             {
                 Console.WriteLine(y.Message);
             }
