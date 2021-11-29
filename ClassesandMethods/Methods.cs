e0c5347e6339851d4c5831f61b83025f82e1ac91
tenchamp31 <ygandhi191@gmail.com>
2021-11-29 12:03:53 -0500

    ClassesMethods

---
+++ ClassesandMethods/Methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    public class ArthematicOperations
+    {
+        public int Add(int data)
+        {
+            data = data + 4; //adding the input
+            return Subtract(data);
+        }
+
+        public int Subtract(int data)
+        {
+            data = data - 3; //subtracting the input after addition
+            return Multiply(data);
+        }
+
+        public int Multiply(int data)
+        {
+            return data * 3; //multiplying input after addition and subtraction
+        }
+    }
+}
---
+++ ClassesandMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        public int Data { get; set; }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Integer:"); //Asking for user input
+            string line = Console.ReadLine();
+            int data = Int32.Parse(line); //converting string to int data type
+
+            var operatorObject = new methods.ArthematicOperations();
+            var answer = operatorObject.Add(data);
+
+
+            Console.WriteLine(answer);
+
+            Console.ReadLine();
+        }
+    }
+}
