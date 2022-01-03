e9cf50fea4c930e5cab62ec3f0fd72988c098678
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-01-03 13:07:56 -0500

    Step 281

---
+++ Step281/ExternalClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281
+{
+    class ExternalClass
+    {
+        public int Add(int x, int y)
+        {
+
+            int sum1 = x + y;
+            return sum1;
+        }
+        
+        public void Add1(int a, int b)
+        {
+            
+            Console.WriteLine(a + b);
+           
+        }
+
+        
+        
+
+    }
+       
+
+    
+    
+}
---
+++ Step281/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step281
+{
+     class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            ExternalClass operatorObject = new ExternalClass();
+
+
+
+            Console.WriteLine("What is your x value");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your y value");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int sum1 = operatorObject.Add(x, y);
+            Console.WriteLine(" Your integers added together is " + operatorObject.Add(x, y) + " " + "second integer is " + y);
+            Console.ReadLine();
+
+
+
+
+            operatorObject.Add1(50,20);
+            Console.ReadLine();
+
+            
+
+           
+
+            
+           
+
+            
+
+        }
+    }
+}
