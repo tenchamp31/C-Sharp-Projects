e1645dab6fce631d92d843a5a96cb7710b1015bf
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-01-03 10:58:37 -0500

    Step280

---
+++ Step280/ExternalClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace RandomThings
+{
+    class ExternalClass
+    {
+
+        public int Add(int num1, [Optional] int num2)
+        {
+            int sum = num1 + num2; //math operation
+            Console.WriteLine(sum);
+            return sum; //returning math operation
+        }
+    }
+
+}
+
+
---
+++ Step280/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace RandomThings
+{
+
+
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+
+            ExternalClass operatorObject = new ExternalClass(); //instance of class
+            Console.WriteLine("What is your favorite number"); //Asking for fav number
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your second fav number or don't reply"); //asking for 2nd fav number
+            int num2;
+            if (int.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine(operatorObject.Add(num1, num2)); //passing method
+                Console.ReadLine();
+            }
+
+
+
+            else
+            {
+                Console.WriteLine(operatorObject.Add(num1));
+                Console.ReadLine();
+
+            }
+
+
+        }
+
+    }
+}
