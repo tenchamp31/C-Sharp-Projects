65be321d271616ab0f81dc50d54c47044182b87c
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-01-04 00:31:59 -0500

    Step 285

---
+++ Step285/ExternalClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step285
+{
+     class ExternalClass
+    {
+       public void Divide(int num1) //creating
+        {
+            int num2;
+            num2 = num1 / 2;
+            Console.WriteLine(num2);
+        }
+
+       
+    }
+
+    public  class Overload //creating method overload
+    {
+        public int Add(int c, int d)
+        {
+            return c + d;
+        }
+
+        public int Add(int c, int d, int e)
+        {
+            return (c + d) + e;
+        }
+
+        
+                
+
+        
+    }
+    }
---
+++ Step285/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step285
+{
+    class Program
+    {
+        public static void Subtract(int a, int b, out int result) //out parameter
+        {
+            result = a - b;
+        }
+
+        public static class School //declaring a static class
+        {
+            public static string SchoolName = "School of Technology";
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a number"); //asking for number
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            var operatorObject = new ExternalClass(); //creating instance of class
+            operatorObject.Divide(num1);
+             //giving out answer
+            Console.ReadLine();
+
+            Subtract(35, 5, out int result);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+            var obj = new Overload(); //method overload
+            var answer1 = obj.Add(20, 30);
+            Console.WriteLine(answer1);
+            Console.ReadLine();
+
+            Console.WriteLine(School.SchoolName);
+            Console.ReadLine();
+        }
+  
+    }
+     
+        
+}
