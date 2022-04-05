c444a7447565074dd36633d388d2bc4e5bcfd087
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-04-05 10:55:57 -0400

    Step 302

---
+++ Step302/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step302
+{
+    public abstract class Person //creating person class
+    {
+        public string FirstName { get; set; } //getting first name
+        public string LastName { get; set; } //getting lastname
+        public abstract void SayName(); //
+    }
+}
---
+++ Step302/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step302
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee(); //creating instance of employee
+            employee.FirstName = "Sample"; //giving firstname of Sample
+            employee.LastName = "Student"; //giving lastname of Student
+            employee.SayName();
+            Console.ReadLine();
+            Console.ReadKey();
+            
+        }
+    }
+}
