67c2e6434935fa87995da46e7594d47ff8bd5442
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-04-06 15:52:30 -0400

    Operator Overloading

---
+++ Operator Overloading/Employees.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    public class Employees
+    {
+        public string Id { get; set; } //Id string
+        public string favcar { get; set; } //string for favcar
+
+        public string Vacation { get; set; } //Vacation string
+
+        
+        
+
+        public static bool operator ==(Employees employee1, Employees employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator !=(Employees employee1, Employees employee2)
+        {
+            return employee1.Id != employee2.Id;
+
+        }
+    }
+
+}
---
+++ Operator Overloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees employee1 = new Employees(); //creating instance of employees
+            Employees employee2 = new Employees();
+            employee1.favcar = "Ford"; //fav car
+            employee2.favcar = "BMW";
+            employee1.Vacation = "Hawaii";
+            employee2.Vacation = "Colorado";
+            employee1.Id = "54354153"; //ID #
+            employee2.Id = "58818822";
+            Console.WriteLine("Does employee1 and employee2 have the same favorite car? " + (employee1.favcar == employee2.favcar));
+            Console.WriteLine("Does employee1 and employee2 have the same vacation spot? " + (employee1.Vacation == employee2.Vacation));
+            Console.WriteLine((employee1 == employee2));
+            Console.ReadKey();
+        }
+    }
+}
