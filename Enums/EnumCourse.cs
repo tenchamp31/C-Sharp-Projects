c5fa54751a978dc4be47d7e30b643f5286894a72
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-04-11 12:48:51 -0400

    Enums

---
+++ Enums/EnumCourse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+     public class EnumCourse
+    {
+        public Days OneDay { get; set; }
+    }
+
+    public enum Days //Days
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+    }
+}
---
+++ Enums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                EnumCourse DayChoice = new EnumCourse(); //creating instance of DayChoice
+                Console.WriteLine("What day is today? "); //asking for the day
+                string currentday = Console.ReadLine();
+                var day = (Days)Enum.Parse(typeof(Days), currentday); //Parse Enum
+                Console.WriteLine("Today is {0} ", (Days)Enum.Parse(typeof(Days), currentday));
+                Console.ReadLine();
+            }
+
+            catch (SystemException e)
+            {
+                Console.WriteLine("Please enter an actual day of the week."); //giiving message for the error
+                Console.ReadLine();
+                return;
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                Console.ReadLine();
+                Console.ReadKey();
+            }
+
+        }
+
+    }
+}
