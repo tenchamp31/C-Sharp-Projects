601ed97abd98e8f4ec7c86385954bb5004348172
tenchamp31 <72271944+tenchamp31@users.noreply.github.com>
2022-04-18 11:53:00 -0400

    ConstructorDrill

---
+++ ConstructorDrill/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDrill
+{
+    public class Employee
+    {
+        public int ID;
+        public string name; //formatting string name
+        public Employee() : this(0, "") { } //chain constructing
+        public Employee(int ID, string name)
+        {
+            this.ID = ID;
+            this.name = name;
+        }
+
+        public Employee(int ID): this(ID, "") { }
+        public Employee(string name): this(0, name) { }
+    }
+}
---
+++ ConstructorDrill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FavCompany = "Apple"; //creating constructor
+            var Year = 2022;  //creating variable with var
+            Employee employee1 = new Employee(1, "Tim Cook");
+            Employee employee2 = new Employee("Craig Ferguson");
+            Console.WriteLine("The best people who work at " + FavCompany + " in "+ Year + " are " + employee1.name +  " and " + employee2.name);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
